d107b3964d7afe81fb0de20f2ce09b3e2284cc66
Baptiste Marmier <baptiste.marmier@ynov.com>
2026-08-07 00:39:59 +0200

    feat(domain): add support operating systems

---
+++ domain/src/main/java/fr/rext/retropcadvisor/domain/model/OperatingSystem.java
@@ -0,0 +1,11 @@
+package fr.rext.retropcadvisor.domain.model;
+
+/**
+ * Systèmes d'exploitation actuellement pris en charge par le moteur de recommandation.
+ */
+public enum OperatingSystem {
+  WINDOWS_95,
+  WINDOWS_98,
+  WINDOWS_98_SE,
+  WINDOWS_XP
+}
